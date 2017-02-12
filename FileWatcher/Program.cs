e2cc9d1ce6deb03ec8a79c344eefaccf0aff2359
Connor Goddard <hello@connorlukegoddard.com>
2017-02-12 16:07:42 +0000

    Initial commit of project files.

---
+++ FileWatcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Run();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public static void Run()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = @"C:\Users\connorgoddard\Desktop\Logs";
+            watcher.Filter = "*.txt";
+
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+            // Wait for the user to quit the program.
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+
+        }
+
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+
+            var trackedFile = new TrackedFile(e.FullPath);
+        }
+
+    }
+}
---
+++ FileWatcher/TrackedFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class TrackedFile
+    {
+        private string filePath;
+        private Timer timer;
+        private Random rand = new Random();
+
+        public TrackedFile(string filePath)
+        {
+            this.filePath = filePath;
+            this.timer = new Timer(CheckForChanges, null, 1000, Timeout.Infinite);
+        }
+
+        public void CheckForChanges(object stateInfo)
+        {
+            Console.WriteLine($"Checking for file changes via timer... - {DateTime.UtcNow}");
+
+            var randTimeout = rand.Next(0, 10) * 1000;
+
+            Console.WriteLine($"Timeout for {randTimeout} secs");
+            Thread.Sleep(randTimeout);
+
+            timer.Change(1000, Timeout.Infinite);
+        }
+    }
+}
